e3b40ce32849e9da793f0ca111b66cdc17171af8
Doug+Roman <doug+roman@appacademy.io>
2014-01-27 08:58:51 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialThing::Application.config.secret_token = '06d251994716e43be3b6b423a73a63e05c9b5569ce569b10e543fae7f532a8fea9b918ecac0c33abcf88da499753652fd4e98d48e68a6f46d8b5560452e8a7eb'
